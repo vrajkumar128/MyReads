0d1992851e10172aa92e3b7eeb7feb0853ee351e
vrrajkum <vivek.rajkumar@protonmail.com>
2018-05-08 16:10:09 -0400

    Add React Router functionality.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    allBooks: [],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    allBooks: []
   }
 
   // Update state with all books currently in collection
